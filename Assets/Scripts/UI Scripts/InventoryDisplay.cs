aee9e619253a32df82879aaaee48243b5af5d427
ELDA7EE7 <mazenmohamedabd@gmail.com>
2025-11-23 00:40:23 +0200

    simple inventory ui done

---
+++ Assets/Scripts/UI Scripts/InventoryDisplay.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public abstract class InventoryDisplay : MonoBehaviour
+{
+    [SerializeField] MouseItemData mouseInventoryItem;
+
+    protected InventorySystem inventorySystem;
+    protected Dictionary<InventorySlot_UI, InventorySlot> slotDictionary;
+    public InventorySystem InventorySystem => inventorySystem;
+    public Dictionary<InventorySlot_UI,InventorySlot> SlotDictionary => slotDictionary;
+    protected virtual void Start()
+    {
+
+    }
+    public abstract void AssignSlot(InventorySystem invToDisplay);
+    public virtual void UpdateSlot(InventorySlot updatedSlot)
+    {
+        foreach(var slot in slotDictionary)
+        {
+            if(slot.Value == updatedSlot) // slot value "under the hood" inventory slot
+            {
+                slot.Key.UpdateUISlot(updatedSlot); // slot key the UI representation of the value
+            }
+        }
+    }
+    public void SlotClicked(InventorySlot_UI clickedSlot)
+    {
+        Debug.Log("Slot clicked");
+    }
+}
