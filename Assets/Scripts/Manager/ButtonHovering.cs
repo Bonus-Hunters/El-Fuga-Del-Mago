4a6e2ae753fd0421980d53f396e2817791daef3e
Nada-Hany <nadaaayman993@gmail.com>
2025-12-05 02:13:05 +0200

    intro screen

--- Assets/Scripts/Manager/ButtonHovering.cs
+++ Assets/Scripts/Manager/ButtonHovering.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using TMPro;
 
-public class ButtonHovering : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class ButtonHovering : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler,
+    IPointerClickHandler
 {
     public TMP_Text text;    // assign your TMP text here
     public CanvasGroup background; // optional if you want to handle transparency
+    private bool isPointerInside = false;
+    public Color normalColor = Color.white;
+    public Color hoverColor = Color.gray;
 
     private void Start()
     {
@@ -16,15 +20,23 @@ public class ButtonHovering : MonoBehaviour, IPointerEnterHandler, IPointerExitH
             background.alpha = 0f;
 
         // Set default text color
-        text.color = Color.white;
+        text.color = normalColor;
     }
     public void OnPointerEnter(PointerEventData eventData)
     {
-        text.color = Color.gray;  // hovered text color
+        isPointerInside = true;
+        text.color = hoverColor;  // hovered text color
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        isPointerInside = false;
         text.color = Color.white; // normal text color
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // Force the color to update *after* the click
+        text.color = normalColor;
     }
 }
--- Assets/Scripts/UI Scripts/PauseMenu.cs
+++ Assets/Scripts/UI Scripts/PauseMenu.cs
@@ -7,6 +7,7 @@ public class PauseMenu : MonoBehaviour
 {
 
     public GameObject Container;
+    public GameObject soundSettingsContainer;
 
     // Update is called once per frame
     void Update()
@@ -27,12 +28,23 @@ public class PauseMenu : MonoBehaviour
 
     }
 
+    public void SoundSettings()
+    {
+
+        Container.SetActive(false);
+        soundSettingsContainer.SetActive(true);
+
+    }
+
+    // should make a transition and load the first scene [intro to the game]
+    // UnityEngine.SceneManagement.SceneManager.LoadScene("name of the scene");
+
 
     public void ExitButton()
     {
-        // should make a transition and load the first scene [intro to the game]
-        // UnityEngine.SceneManagement.SceneManager.LoadScene("name of the scene");
-
+        Destroy(Container);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("IntroScene");
+        Time.timeScale = 1;
     }
 
 }
